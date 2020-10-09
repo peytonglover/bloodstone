2b164ecc152cf8ea9c96ef6bbfade90d7f5fc3f1
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-08 20:54:02 -0400

    complete serializers

--- survey/models.py
+++ survey/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Survey(models.Model):
     title = models.TextField(max_length=140)
+    # author = models.ForeignKey('user', on_delete=models.CASCADE, related_name='creator')
     description = models.TextField(max_length=300, null=True, blank=True)
     question = models.ManyToManyField('Question', related_name='surveys_question', blank=True)
     private_flag = models.BooleanField(default=False)
@@ -27,7 +28,7 @@ class Question(models.Model):
     )
 
     question_body = models.CharField(max_length=250)
-    survey = models.ForeignKey(Survey, null=True, blank=True, on_delete=models.CASCADE, related_name='questions')
+    survey = models.ManyToManyField(Survey, blank=True, related_name='questions')
     question_type = models.CharField(max_length=250, choices=TYPE_QUESTION, default=TYPE_QUESTION_TEXT)
 
     def __str__(self):
--- survey/serializers.py
+++ survey/serializers.py
@@ -1,2 +1,47 @@
 from rest_framework import serializers
-from survey.models import Survey, Question, Choice,  
+from survey.models import Survey, Question, Choice, Result
+
+class SurveySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Survey
+        fields = [
+            'id',
+            'title',
+            # 'author',
+            'description',
+            'question',
+            'private_flag',
+            'survey_link',
+            'results',
+        ]
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = [
+            'id',
+            'question_body',
+            'survey',
+            'question_type',
+        ]
+
+class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Choice
+        fields = [
+            'id',
+            'question',
+            'choice_body',
+        ]
+
+class ResultSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Result
+        fields = [
+            'id',
+            'question',
+            # 'user',
+            'answer',
+        ]
+
