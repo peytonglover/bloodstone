bd0aadf7b7724d276aeef01c36ae24007dcf08aa
Andrew Fillenwarth <andyf016@gmail.com>
2020-10-13 01:15:44 -0400

    finish filter by private flag actions

--- survey/views.py
+++ survey/views.py
@@ -16,6 +16,29 @@ class SurveyViewSet(viewsets.ModelViewSet):
     queryset = Survey.objects.all().order_by('-created')
     serializer_class = SurveySerializer
 
+    @action(detail=False)
+    def public(self, request):
+        results = Survey.objects.filter(private_flag=False)
+
+        page = self.paginate_queryset(results)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(results, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False)
+    def private(self, request):
+        results = Survey.objects.filter(private_flag=True)
+
+        page = self.paginate_queryset(results)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(results, many=True)
+        return Response(serializer.data)
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
